7d42af58f17e28dbc41e895a57f74cf002578ed5
Golu <anurag.bhardwaj.cpp@gmail.com>
2020-07-07 21:40:00 +0530

    Updates

---
+++ precache-manifest.af62e50a0c55d2fdcd0b8afeab22bfb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed76fae32515fc8a9a14ea2534d6955d",
+    "url": "/timetable/index.html"
+  },
+  {
+    "revision": "4d640b00fb7d7bc77af2",
+    "url": "/timetable/static/css/main.77620376.chunk.css"
+  },
+  {
+    "revision": "3ac1b2086d93a3911622",
+    "url": "/timetable/static/js/2.de02fc28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/timetable/static/js/2.de02fc28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d640b00fb7d7bc77af2",
+    "url": "/timetable/static/js/main.ec56bef0.chunk.js"
+  },
+  {
+    "revision": "b679e5cf8c0134a639b5",
+    "url": "/timetable/static/js/runtime-main.97208804.js"
+  }
+]);
